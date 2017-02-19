1b15ab1af8b7902294b20dc379e34a15a4f11233
Tuan Nguyen Duc <tuannd.bkgm@gmail.com>
2017-02-20 03:28:45 +0700

    final push

--- Client-QT-GUI/sockettest.cpp
+++ Client-QT-GUI/sockettest.cpp
@@ -18,18 +18,11 @@ void SocketTest::Test()
 {
     socket = new QTcpSocket(this);
     connect(socket, SIGNAL(connected()), this, SLOT(connected()));
-//    connect(socket, SIGNAL(disconnected()), this, SLOT(disconnected()));
     connect(socket, SIGNAL(readyRead()), this, SLOT(readyRead()));
     connect(socket, SIGNAL(bytesWritten(qint64)), this, SLOT(bytesWritten(qint64)));
 
     qDebug() << "Connecting,..";
-
     socket->connectToHost(host, port);
-
-    if(!socket->waitForDisconnected(1000))
-    {
-        qDebug() << "Error: " << socket->errorString();
-    }
 
 }
 
@@ -59,9 +52,6 @@ void SocketTest::readyRead()
     qDebug() << "Reading...";
     QString message = socket->readAll();
     qDebug() << message;
-//    MainWindow *w = new MainWindow();
-    // Send signal to UI
-//    connect(this, SIGNAL(signalUpdateUI(QString)), w, SLOT(updateListMessage(QString)));
     readyUpdateUI(message);
 }
 
