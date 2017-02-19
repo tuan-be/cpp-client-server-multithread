826b89587a079c97dc984b14c55ee9389b143e31
Tuan Nguyen Duc <tuannd.bkgm@gmail.com>
2017-02-19 11:14:56 +0700

    The first commit, add server code

---
+++ Server/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "mythread.h"
+#include "server.h"
+
+using namespace std;
+
+int main() {
+  cout << "Starting server at host 127.0.0.1 and port 6969!" << endl;
+
+  Server *s;
+  s = new Server();
+
+  //Main loop
+  s->AcceptAndDispatch();
+
+  return 0;
+}
